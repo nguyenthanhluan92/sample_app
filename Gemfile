62e0c9085add353b4f83726240a3fb25482c3a61
huyenmin304 <huyen23813@gmai.com>
2019-01-16 19:32:14 +0700

    finish index, show, edit, update, destroy action

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ ruby '2.5.3'
 gem 'bcrypt',         '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
 gem 'jquery-rails'
+gem 'faker'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :authenticate, only: [:edit, :update]
+  before_action :authenticate, only: [:index, :show, :edit, :update, :delete]
   before_action :check_login, only: [:new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :verify_admin, only: [:destroy]
 
   def new
     @user = User.new
@@ -17,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def show
@@ -25,26 +27,53 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Cap nhat thong tin ca nhan thanh cong!"
+      redirect_to @user
+    else
+      flash[:warning] = "Cap nhat khong thanh cong!"
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "Xoa tai khoan thanh cong!"
+    redirect_to users_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def authenticate
-    current_user = User.find_by(id: session[:user_id])
-    return if (current_user == User.find(params[:id]))
+    return if logged_in?
+    flash[:warning] = "Ban khong co quyen truy cap!"
+    redirect_to root_url
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    return if @user && (@current_user == @user)
     flash[:warning]= "Ban khong co quyen chinh sua trang ca nhan nguoi khac!"
-    redirect_to current_user
+    redirect_to @current_user
   end
 
   def check_login
     return unless logged_in?
     flash[:warning] = "Ban khong co quyen truy cap trang nay!"
     redirect_to current_user
+  end
+
+  def verify_admin
+    return if current_user.admin?
+    redirect_to root_url
   end
 
 end
